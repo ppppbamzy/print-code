279c1fe7d2caaa0e4e59c13b9b9a05046a35a295
ppppbamzy <pppp.bamzy@gmail.com>
2020-07-25 22:14:33 +0800

    Update split.sh

--- split.sh
+++ split.sh
@@ -1,1 +1,9 @@
 gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -sPageList=odd -sOutputFile=0__odd.pdf $1
+pl=""
+for((i=$2;i>0;i-=2))
+do
+  inv=$(printf "%03d" $(($2-i)))
+  # echo $inv
+  gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -sPageList=$i -sOutputFile=even_$inv $1
+done
+gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -sOutputFile=1_even.pdf even*
